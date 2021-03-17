ab17109ca7b0719314aa82ff57eb2bc7f8b4e972
Tsvetelinna <cvetilinna@gmail.com>
2021-03-17 13:42:58 +0200

    Add solutions for task01

---
+++ cs-4-exercise/week03 - Unit Testing/solutions/task01/Main.cpp
@@ -0,0 +1,39 @@
+#include "DynArray.h"
+#include <iostream>
+//
+//void pushBackTest() {
+//	DynArray dynArray = create();
+//	push_back(dynArray, 10);
+//	push_back(dynArray, 20);
+//
+//	if (get(dynArray, 0) == 10) {
+//		std::cout << "Test is successful" << std::endl;
+//	}
+//	else {
+//		std::cout << "Test fail" << std::endl;
+//	}
+//
+//}
+
+
+//int main() {
+//
+//	/*DynArray dynArray = create();
+//	push_back(dynArray, 10);
+//	push_back(dynArray, 20);
+//
+//	std::cout << get(dynArray, 1) << std::endl;
+//
+//	pop_back(dynArray);
+//
+//	std::cout << get(dynArray, 0) << std::endl;
+//	eraseDynMemory(dynArray);
+//
+//	std::cout << get(dynArray, 0) << std::endl;*/
+//
+//	pushBackTest();
+//
+//
+//
+//	return 0;
+//}
