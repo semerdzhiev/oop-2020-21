267cc4296ff5cacb4caa3c905318ce66c9e404e7
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2021-03-19 15:46:02 +0200

    Add struct wrapper over variables

--- inf-1-exercise/week3/Interp/Source.cpp
+++ inf-1-exercise/week3/Interp/Source.cpp
@@ -1,6 +1,9 @@
-﻿#include <cstring>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
 #include <iostream>
 /*
+Constructor/Destructor (class/struct)
+
 Входа винаги е коректен
 Променливите са винаги целочислени
 Имената на променливите са < 32
@@ -26,63 +29,69 @@ calc size % size
 
 typedef char VariableName[32];
 const int maxCount = 200;
-
-struct Variable {
+struct UserVariable {
 	VariableName name;
-	int value;
-	bool init;
+	int value = 0;
+	bool init = false;
+
+	UserVariable() {}
+	//UserVariable() = default;
+
+	UserVariable(VariableName varName, int varValue, bool varInit)
+		: value(varValue), init(varInit) {
+		strcpy(name, varName);
+	}
 };
 
-int findVariableIndex(VariableName names[], int variableCount, VariableName search){
+struct VariableCollection {
+	UserVariable variables[maxCount];
+	int variableCount = 0;
+
+	void declareVariable(VariableName name) {
+		strcpy(variables[variableCount].name, name);
+		std::cout << "Declared variable " << variables[variableCount].name << std::endl;
+		++variableCount;
+	}
+};
+
+int findVariableIndex(UserVariable vars[], int variableCount, VariableName search) {
+
 	for (int c = 0; c < variableCount; ++c) {
-		if (strcmp(names[c], search) == 0) {
+		if (strcmp(vars[c].name, search) == 0) {
 			return c;
 		}
 	}
 	return -1;
 }
 
-void declareVariable(bool initVariables[], VariableName variableNames[], int &variableCount, VariableName name) {
 
-	//strcpy(variableNames[variableCount], name);
-	int c = 0;
-	for (; name[c] != '\0'; c++) {
-		variableNames[variableCount][c] = name[c];
-	}
-	variableNames[variableCount][c] = '\0';
 
-	initVariables[variableCount] = false;
-	std::cout << "Declared variable " << variableNames[variableCount] << std::endl;
-	++variableCount;
-}
-
-void assignVariable(VariableName variableNames[], int variableValues[],
-	bool initVariables[], int &variableCount, VariableName name, int value)
-{
-	const int index = findVariableIndex(variableNames, variableCount, name);
-	initVariables[index] = true;
-	variableValues[index] = value;
+void assignVariable(UserVariable vars[], int &variableCount, VariableName name, int value) {
+	const int index = findVariableIndex(vars, variableCount, name);
+	vars[index].init = true;
+	vars[index].value = value;
 	std::cout << "Assigned variable " << name << " = " << value << std::endl;
 }
 
-void printVariable(VariableName variableNames[], int variableValues[],
-	bool initVariables[], int &variableCount, VariableName name)
+void printVariable(UserVariable vars[], int &variableCount, VariableName name)
 {
-	const int index = findVariableIndex(variableNames, variableCount, name);
-	if (initVariables[index]) {
-		std::cout << variableValues[index] << std::endl;
+	const int index = findVariableIndex(vars, variableCount, name);
+	if (vars[index].init) {
+		std::cout << vars[index].value << std::endl;
 	} else {
 		std::cout << "Undefined variable " << name << std::endl;
 	}
 }
 
 int main() {
-	// Variable variables[maxCount];
-	int variableValues[maxCount];
-	VariableName variableNames[maxCount];
-	bool initVariables[maxCount];
+	VariableCollection collection;
 
-	int variableCount = 0;
+	// UserVariable variables[maxCount];
+	//int variableValues[maxCount];
+	//VariableName variableNames[maxCount];
+	//bool initVariables[maxCount];
+
+	//int variableCount = 0;
 
 	char command[32];
 	do {
@@ -90,16 +99,17 @@ int main() {
 		if (!strcmp(command, "declare")) {
 			VariableName name;
 			std::cin >> name;
-			declareVariable(initVariables, variableNames, variableCount, name);
+			collection.declareVariable(name);
+			// declareVariable(collection.variables, collection.variableCount, name);
 		} else if (!strcmp(command, "assign")) {
 			VariableName name;
 			int value;
 			std::cin >> name >> value;
-			assignVariable(variableNames, variableValues, initVariables, variableCount, name, value);
+			assignVariable(collection.variables, collection.variableCount, name, value);
 		} else if (!strcmp(command, "print")) {
 			VariableName name;
 			std::cin >> name;
-			printVariable(variableNames, variableValues, initVariables, variableCount, name);
+			printVariable(collection.variables, collection.variableCount, name);
 		} else if (!strcmp(command, "calc")) {
 
 		}
