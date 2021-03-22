d46266168f0611998dc9e4496f3b91e85e9ee224
5ko99 <petko.krasimirov99@gmail.com>
2021-03-22 17:47:20 +0200

    Exercise 4: Partial solution of Dealarship task.

---
+++ cs-2-practical/04-unit-tests-and-more-tasks/CarDealership/VisualDetails.cpp
@@ -0,0 +1,2 @@
+#include "VisualDetails.h"
+
---
+++ cs-2-practical/04-unit-tests-and-more-tasks/CarDealership/cmake-build-debug/CMakeFiles/CarDealership.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarDealership.dir/Car.cpp.o"
+  "CMakeFiles/CarDealership.dir/Dealership.cpp.o"
+  "CMakeFiles/CarDealership.dir/EngineDetails.cpp.o"
+  "CMakeFiles/CarDealership.dir/ManufactureDetails.cpp.o"
+  "CMakeFiles/CarDealership.dir/VisualDetails.cpp.o"
+  "CMakeFiles/CarDealership.dir/main.cpp.o"
+  "CarDealership"
+  "CarDealership.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarDealership.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
